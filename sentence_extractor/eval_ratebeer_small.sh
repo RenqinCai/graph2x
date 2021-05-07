30c4a34d83b54f92382003898452f24980431e7f
rc7ne <rc7ne@hcdmg2.cs.virginia.edu>
2021-05-06 23:35:18 -0400

    add sh files

--- sentence_extractor/eval_ratebeer_small.sh
+++ sentence_extractor/eval_ratebeer_small.sh
@@ -1,4 +1,6 @@
-CUDA_VISIBLE_DEVICES=1 python main.py --data_dir "/p/reviewde/data/ratebeer/graph/small_500/" --graph_dir "/p/reviewde/data/ratebeer/graph/small_500/graph_batch/" --epoch_num 5 --eval --batch_size 8 --learning_rate 0.0001 --optimizer "Adam" --user_embed_size 256 --item_embed_size 256 --feature_embed_size 256 --sent_embed_size 768 --hidden_size 256 --head_num 4 --ffn_inner_hidden_size 256 --ffn_dropout_rate 0.01 --attn_dropout_rate 0.01 --model_file "ratebeer_graph_sentence_extractor/model_best_4_29_13_19.pt"
+CUDA_VISIBLE_DEVICES=1 python main.py --data_dir "/p/reviewde/data/ratebeer/graph/small_500_v2/" --graph_dir "/p/reviewde/data/ratebeer/graph/small_500_v2/graph_batch/" --epoch_num 5 --eval --batch_size 64 --learning_rate 0.0001 --optimizer "Adam" --user_embed_size 256 --item_embed_size 256 --feature_embed_size 256 --sent_embed_size 768 --hidden_size 256 --head_num 4 --ffn_inner_hidden_size 256 --ffn_dropout_rate 0.01 --attn_dropout_rate 0.01 --model_file "ratebeer_graph_sentence_extractor/model_best_5_4_8_50.pt"
 
+
+# hcdmg2_05_04_08_50
 # model_best_4_24_22_13.pt
 # model_best_4_24_15_52.pt
