b819fea9f38feeb959eaaea4a7e3781432275c3f
rc7ne <rc7ne@hcdmg2.cs.virginia.edu>
2021-07-13 10:34:37 -0400

    add flag to control whether print attention weight or not

--- geometric_sentence_extractor/GAT.py
+++ geometric_sentence_extractor/GAT.py
@@ -13,16 +13,26 @@ class GATNET(torch.nn.Module):
         # default, concat all attention head
         self.gat_layer_2 = GATConv(head_num*out_dim, out_dim, heads=1, concat=False, dropout=dropout_rate)
     
-    def forward(self, x, edge_index):
+    def forward(self, x, edge_index, attention_flag=False):
         x_0 = F.dropout(x, p=0.6, training=self.training)
         # x = F.elu(self.gat_layer_1(x, edge_index))
         
+        if attention_flag:
         ### e_1: edge attention weights (edge_index, attention_weights) 
-        x_1, e_1 = self.gat_layer_1(x_0, edge_index, return_attention_weights=True)
+            x_1, e_1 = self.gat_layer_1(x_0, edge_index, return_attention_weights=True)
+        else:
+            x_1 = self.gat_layer_1(x_0, edge_index, return_attention_weights=True)
         x_1 = F.elu(x_1)
 
         x_1 = F.dropout(x_1, p=0.6, training=self.training)
-        x_2, e_2 = self.gat_layer_2(x_1, edge_index, return_attention_weights=True)
 
-        return x_2
+        if attention_flag:
+            x_2, e_2 = self.gat_layer_2(x_1, edge_index, return_attention_weights=True)
+        else:
+            x_2 = self.gat_layer_2(x_1, edge_index, return_attention_weights=True)
+
+        if attention_flag:
+            return x_2, e_1, e_2
+        else:
+            return x_2
 
